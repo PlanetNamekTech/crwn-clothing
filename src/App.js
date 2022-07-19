f4f8c7e6ea26537d559613b49bcd7eae569ebc04
PlanetNamekTech <sr23513@gmail.com>
2022-07-18 17:44:50 -0700

    added shop page skeleton

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './componenents/routes/home/home.component';
 import Navigation from './componenents/routes/navigation/navigation.component';
 import Authentication from './componenents/routes/authentication/authentication.component';
+import Shop from './componenents/routes/shop/shop.component';
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
